7a74464755f4a2a392ffe4db1cf26cf659710e30
sarasadig87 <sarakhalifa491@gmail.com>
2023-03-28 17:30:34 -0700

    first commit

---
+++ src/main/java/com/maven/collab/NEW_MAVEN_H/App.java
@@ -0,0 +1,13 @@
+package com.maven.collab.NEW_MAVEN_H;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
